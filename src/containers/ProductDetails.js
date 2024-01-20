b64b52eb6c45686bb434dcc526bff45c6e1aec50
divya661 <divyachauhan@MacBook-Pro-2.local>
2024-01-21 00:37:14 +0530

    first commit

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,31 +1,24 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  selectedProduct,
   removeSelectedProduct,
+  fetchProductDetail
 } from "../redux/actions/productsActions";
+
 const ProductDetails = () => {
   const { productId } = useParams();
   let product = useSelector((state) => state.product);
   const { image, title, price, category, description } = product;
   const dispatch = useDispatch();
-  const fetchProductDetail = async (id) => {
-    const response = await axios
-      .get(`https://fakestoreapi.com/products/${id}`)
-      .catch((err) => {
-        console.log("Err: ", err);
-      });
-    dispatch(selectedProduct(response.data));
-  };
-
+  
   useEffect(() => {
-    if (productId && productId !== "") fetchProductDetail(productId);
+    if (productId && productId !== "") dispatch(fetchProductDetail(productId));
     return () => {
       dispatch(removeSelectedProduct());
     };
-  }, [productId]);
+  }, [productId,dispatch]);
+
   return (
     <div className="ui grid container">
       {Object.keys(product).length === 0 ? (
